b03dcd9c16a1b99ea819d81a6459390ef7ca56a7
Shweta Shaw <189984154+ShwetaShaw76@users.noreply.github.com>
2025-12-12 12:49:57 +0000

    Adding textures

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
-import * as THREE from 'three'; 
-import {OrbitControls} from 'three/addons/controls/OrbitConstrols.js';
-import {GLTFLoader} from 'three/addons/controls/GLTFLoader.js';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+// import { cameraPosition } from 'three/tsl';
 
-const controls = new OrbitControls(camera, renderer.domElement);
 const loader = new GLTFLoader();
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+const controls = new OrbitControls(camera, renderer.domElement);
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.set(0,0,5);
+camera.lookAt(0,0,0);
+
+function animate() {
+    cube.rotation.x+=0.01;
+    cube.rotation.y+=0.01;
+    renderer.render(scene, camera);
+}
+
+
+const points =[];
+points.push(new THREE.Vector3(-10,0,0));
+points.push(new THREE.Vector3(0,10,0));
+points.push(new THREE.Vector3(10,0,0));
+
+const geometry1 = new THREE.BufferGeometry.setFromPoints(points);
+
+const line = new THREE.Line(geometry1,material);
+
+scene.add(line);
+
+renderer.setAnimationLoop(animate);
